5d5864862cb2d60ee5d84a25b2cb255cd8e9271c
eloomati <hetko.mateusz@gmail.com>
2025-07-27 00:02:11 +0200

    Addition of test properties

--- src/test/java/io/mhetko/lor/LorApplicationTests.java
+++ src/test/java/io/mhetko/lor/LorApplicationTests.java
@@ -2,8 +2,10 @@ package io.mhetko.lor;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LorApplicationTests {
 
     @Test
