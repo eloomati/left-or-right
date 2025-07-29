c19492b7cea578f3068257ecc4972a25b5c5fb0c
eloomati <hetko.mateusz@gmail.com>
2025-07-29 23:51:36 +0200

    Improving MailService and adding logging

--- src/main/java/io/mhetko/lor/service/MailService.java
+++ src/main/java/io/mhetko/lor/service/MailService.java
@@ -1,26 +1,39 @@
 package io.mhetko.lor.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.lang.NonNull;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class MailService {
 
     private final JavaMailSender mailSender;
 
-    public void sendConfirmationEmail(String to, String token) {
-        String subject = "Potwierdzenie rejestracji";
+    public void sendConfirmationEmail(@NonNull String to, @NonNull String token) {
+        String subject = "Registration confirmation";
         String confirmationUrl = "http://localhost:8080/api/users/confirm?token=" + token;
-        String text = "Kliknij w link, aby potwierdzić rejestrację: " + confirmationUrl;
+        String text = "Click the link to confirm your registration: " + confirmationUrl;
+        sendEmail(to, subject, text);
+    }
 
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(text);
+    private void sendEmail(@NonNull String to, @NonNull String subject, @NonNull String text) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(text);
 
-        mailSender.send(message);
+            log.info("Sending email to: {}, subject: {}", to, subject);
+            mailSender.send(message);
+            log.info("Email sent to: {}", to);
+        } catch (Exception e) {
+            log.error("Error while sending email to: {}", to, e);
+            throw e;
+        }
     }
 }
