69e55c5f753e2b6498137219bd51c4843158eed1
eloomati <hetko.mateusz@gmail.com>
2025-07-29 23:43:54 +0200

    Adding a filter for request and response logging

---
+++ src/main/java/io/mhetko/lor/filter/LoggingFilter.java
@@ -0,0 +1,41 @@
+package io.mhetko.lor.filter;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@Component
+@Slf4j
+public class LoggingFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        ContentCachingRequestWrapper wrappedRequest = new ContentCachingRequestWrapper((HttpServletRequest) request);
+        ContentCachingResponseWrapper wrappedResponse = new ContentCachingResponseWrapper((HttpServletResponse) response);
+
+        log.info("Incoming request: {} {}", wrappedRequest.getMethod(), wrappedRequest.getRequestURI());
+
+        chain.doFilter(wrappedRequest, wrappedResponse);
+
+        String requestBody = new String(wrappedRequest.getContentAsByteArray(), StandardCharsets.UTF_8);
+        String responseBody = new String(wrappedResponse.getContentAsByteArray(), StandardCharsets.UTF_8);
+
+        log.info("Request body: {}", requestBody);
+        log.info("Outgoing response: {}", wrappedResponse.getContentType());
+        log.info("Response body: {}", responseBody);
+
+        wrappedResponse.copyBodyToResponse();
+    }
+}
