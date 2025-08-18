75e31ea889873353c1034cb439ac7dd1c0816428
eloomati <hetko.mateusz@gmail.com>
2025-08-18 23:10:46 +0200

    Delete proposed_topic_watch

--- src/main/java/io/mhetko/lor/repository/ProposedTopicWatchRepository.java
+++
@@ -1,9 +0,0 @@
-package io.mhetko.lor.repository;
-
-import io.mhetko.lor.entity.ProposedTopicWatch;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ProposedTopicWatchRepository extends JpaRepository<ProposedTopicWatch, Long> {
-    List<ProposedTopicWatch> findAllByProposedTopicId(Long proposedTopicId);
-}
